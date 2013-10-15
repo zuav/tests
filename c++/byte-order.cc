1d92d9659351d8a041ae882fa5b10e080823bd19
Alexander Zhukov <zuav@crystax.net>
2013-10-15 12:07:49 +0200

    Use BYTE_ORDER, not __BYTE_ORDER.

--- c++/byte-order.cc
+++ c++/byte-order.cc
@@ -1,13 +1,27 @@
-#include <endian.h>
+//#include <machine/endian.h>
+
+#include <sys/types.h>
 #include <iostream>
 
-#ifndef __BYTE_ORDER
-#error __BYTE_ORDER is undefined!
+// #ifndef __BYTE_ORDER
+// #error __BYTE_ORDER is undefined!
+// #endif
+
+// #if __BYTE_ORDER == __BIG_ENDIAN
+// const char *byte_order = "big endian";
+// #elif __BYTE_ORDER == __LITTLE_ENDIAN
+// const char *byte_order = "little endian";
+// #else
+// const char *byte_order = "unknown byte order";
+// #endif
+
+#ifndef BYTE_ORDER
+#error BYTE_ORDER is undefined!
 #endif
 
-#if __BYTE_ORDER == __BIG_ENDIAN
+#if BYTE_ORDER == BIG_ENDIAN
 const char *byte_order = "big endian";
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
+#elif BYTE_ORDER == LITTLE_ENDIAN
 const char *byte_order = "little endian";
 #else
 const char *byte_order = "unknown byte order";
