bd844362138d3f8f8ca18488b66b739c2692de04
Alexander Zhukov <zuav@crystax.net>
2013-10-22 16:15:18 +0200

    Add output as uint64_t.

--- pthreads/pthread-t.cc
+++ pthreads/pthread-t.cc
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <pthread.h>
 #include <iostream>
 #include <iomanip>
@@ -7,7 +8,8 @@ int main()
     pthread_t tid = pthread_self();
 
     std::cout << tid << std::endl
-              << std::hex << tid << std::endl;
+              << std::hex << tid << std::endl
+              << std::setw(16) << std::setfill('0') << std::hex << uint64_t(tid) << std::endl;
 
     return 0;
 }
