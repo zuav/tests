c2d4122b3c5228744315e3e5a3ed8194892b37c2
Alexander Zhukov <azhukov@intermedia.net>
2020-06-02 17:44:24 +0300

    Add strlen-null.cc test

---
+++ libc/strlen-null.cc
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+    long l = strlen(0);
+    std::cout << "length of NULL string = " << l << '\n';
+
+    return 0;
+}
